0c3d5671486b4d6f77f39d5556563e9eb09d33d4
joseumanzor <jm.umanzor10@gmail.com>
2024-09-18 20:08:41 -0400

    Update StandardTrackingWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadrunnerUtilStuff/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadrunnerUtilStuff/drive/StandardTrackingWheelLocalizer.java
@@ -51,9 +51,9 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         lastEncPositions = lastTrackingEncPositions;
         lastEncVels = lastTrackingEncVels;
 
-        //leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "mtrFL"));
-        //rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "mtrFR"));
-        //frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "mtrBL"));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "m4"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "m2"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "m1"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
         frontEncoder.setDirection(Encoder.Direction.FORWARD);
