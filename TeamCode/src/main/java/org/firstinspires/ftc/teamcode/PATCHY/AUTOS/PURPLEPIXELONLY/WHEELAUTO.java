fc77c85f77d3886eea16eec57fb04946c137cd23
SiEclipzi <110932027+SiEclipzi@users.noreply.github.com>
2024-10-14 18:24:31 -0400

    Made a base auto and testing wheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PATCHY/AUTOS/PURPLEPIXELONLY/WHEELAUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PATCHY/AUTOS/PURPLEPIXELONLY/WHEELAUTO.java
@@ -3,17 +3,16 @@ package org.firstinspires.ftc.teamcode.PATCHY.AUTOS.PURPLEPIXELONLY;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.PATCHY.hardwareCS;
+import org.firstinspires.ftc.teamcode.RoadrunnerUtilStuff.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
 
-@Disabled
 @Config
-@Autonomous(name = "Red Purple Pixel", group = "Purple Pixel Autos")
-public class BaseAuto extends LinearOpMode {
+@Autonomous(name = "Wheel Testing Auto", group = "Test Autos")
+public class WHEELAUTO extends LinearOpMode {
 
     private VisionPortal portal;
     private org.firstinspires.ftc.teamcode.PATCHY.PIPELINES.redpropPipeline redpropPipeline;
@@ -31,6 +30,11 @@ public class BaseAuto extends LinearOpMode {
 
         Pose2d startPose = new Pose2d(0,0, Math.toRadians(0));
 
+        TrajectorySequence move = drive.trajectorySequenceBuilder(startPose)
+                .forward(60)
+                .turn(Math.toRadians(60))
+                .build();
+
         while (!isStarted() && !isStopRequested()) {
 
             telemetry.addLine("waitForStart");
@@ -42,6 +46,11 @@ public class BaseAuto extends LinearOpMode {
         if (isStopRequested()) return;
 
         drive.setPoseEstimate(new Pose2d(22,0,Math.toRadians(0)));
+        telemetry.addData("Updated start pose estimate: ", drive.getPoseEstimate());
+        telemetry.update();
+        drive.followTrajectorySequence(move);
+        telemetry.addData("Currently at: ", drive.getPoseEstimate());
+        telemetry.update();
 
         while (!isStopRequested() && opModeIsActive()) ;
 
